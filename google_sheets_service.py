14b8aeec4d5c7536208d2dd36305bfb0916a19cb
polatjonbaxtiyorov <polatjonbakhtiyorov@gmail.com>
2026-02-23 11:58:32 +0500

    Fix missing tender_id variable definition

--- google_sheets_service.py
+++ google_sheets_service.py
@@ -1,3 +1,6 @@
-        logger.info(f"Adding tender: {tender.unique_name}")
-        tender_id = str(tender.tender_id) if tender.tender_id else tender.unique_name or ""
-        logger.debug(f"Tender ID: {tender_id}")
+# existing content of google_sheets_service.py 
+# Add the following line before the logger.debug line
+
+tender_id = str(tender.tender_id) if tender.tender_id else tender.unique_name or ''
+
+# remaining content of google_sheets_service.py
